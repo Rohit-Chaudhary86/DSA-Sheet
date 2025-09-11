de07c999d2c6090955d7218fae44c25fca83564f
Rohit chaudhary <151979116+Rohit-Chaudhary86@users.noreply.github.com>
2025-09-11 14:46:50 +0530

    Solved Q no 509 leetcode DP approach

---
+++ DP/FibonacciNumber.js
@@ -0,0 +1,15 @@
+// Q= 509. Fibonacci Number  leetcode
+
+
+let dp;
+
+function f(n){
+    if(n===0 || n===1)return n
+    if(dp[n]!=-1)return dp[n]
+    return dp[n]= f(n-1)+f(n-2)
+}
+
+var fib = function(n) {
+    dp=Array(n+1).fill(-1)
+    return f(n)
+};
